4310b6b0f7dde62c54f974c1404cc480c721d238
Haley Collard <collard.haley20@gmail.com>
2020-07-04 14:42:18 -0400

    part A done

--- iss.py
+++ iss.py
@@ -1,10 +1,21 @@
 #!/usr/bin/env python
 
-__author__ = '???'
+__author__ = 'Haley Collard'
+
+import requests
+
+
+def get_astronauts():
+    r = requests.get('http://api.open-notify.org/astros.json')
+    resp_list = r.text.split('[')
+    number = resp_list[0].split(',')
+    number = number[1]
+    print(f'There are {number[-1:]} astronauts currently in space.')
+    print(f'There names and crafts names are... {resp_list[1]}')
 
 
 def main():
-    pass
+    get_astronauts()
 
 
 if __name__ == '__main__':
