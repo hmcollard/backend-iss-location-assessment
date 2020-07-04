c6a77e2e3c361807531449bfa1c9c750cf89e514
Haley Collard <collard.haley20@gmail.com>
2020-07-04 19:11:10 -0400

    part A and part B working

--- iss.py
+++ iss.py
@@ -2,20 +2,32 @@
 
 __author__ = 'Haley Collard'
 
+import re
+import json
 import requests
 
 
 def get_astronauts():
     r = requests.get('http://api.open-notify.org/astros.json')
-    resp_list = r.text.split('[')
-    number = resp_list[0].split(',')
-    number = number[1]
-    print(f'There are {number[-1:]} astronauts currently in space.')
-    print(f'There names and crafts names are... {resp_list[1]}')
+    data = json.loads(r.text)
+    # print(data)
+    print(f'There are {data.get("number")} astronauts currently in space.')
+    print(f'There names and crafts names are... {data.get("people")}')
 
+
+def get_coordinates():
+    ISS_dict = {}
+    r = requests.get('http://api.open-notify.org/iss-now.json')
+    data = r.text
+    matches = re.findall(r'-?\d+\.?\d+', data)
+    ISS_dict['timestamp'] = matches[0]
+    ISS_dict['latitude'] = matches[1]
+    ISS_dict['longitude'] = matches[2]
+    return ISS_dict
 
 def main():
     get_astronauts()
+    # get_coordinates()
 
 
 if __name__ == '__main__':
